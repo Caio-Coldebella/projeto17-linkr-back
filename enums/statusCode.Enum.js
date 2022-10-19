9b4209cebb14d6c49e7ccc85227f7628d14db71b
Aleisson <caioaleisson@gmail.com>
2022-10-18 22:50:27 -0300

    docs(enums/statusCode): create status code

---
+++ enums/statusCode.Enum.js
@@ -0,0 +1,17 @@
+const STATUS_CODE = Object.freeze({
+
+    OK: 200,
+    CREATED: 201,
+    NOT_CONTENT: 204,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    UNAUTHORIZED: 401,
+    CONFLICT: 409,
+    UNPROCESSABLE_ENTITY: 422,
+    SERVER_ERROR: 500,
+    NOT_IMPLEMENTED: 501
+
+
+});
+
+export { STATUS_CODE };
