b448b0f5a0c22aa01454dd0ac7b7aa42bf798ac0
isael lopes <isael.alves@turucui.ufpa.br>
2022-10-22 16:02:07 -0300

    atualizando importação rota hashtag

--- repositories/hashtagRepository.js
+++ repositories/hashtagRepository.js
@@ -1,12 +1,14 @@
 import Sqlstrng from 'sqlstring';
 
-import {connectDataBase} from '../database/database.js';
+import db from '../database/database.js';
 
-export async function findHashtag() {
-    const find = connectDataBase.query(`SELECT * FROM topicsPosts;`); 
+async function findHashtag() {
+    const find = db.query(`SELECT * FROM topicsPosts;`); 
     return find;
 }
 
-export async function findHashtagId(){
-    return connectDataBase.query(`SELECT *from topicsPosts WHERE id ${Sqlstrng.escape(id)}`);
+async function findHashtagId(){
+    return db.query(`SELECT *from topicsPosts WHERE id ${Sqlstrng.escape(id)}`);
 }
+
+export { findHashtag, findHashtagId };
