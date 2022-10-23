69fdc25d200e0c0b20df22a940866573368269ea
isael lopes <isael.alves@turucui.ufpa.br>
2022-10-23 17:44:35 -0300

    rota de busca por usuario incompleta

--- controllers/seachUserControler.js
+++ controllers/seachUserControler.js
@@ -2,16 +2,12 @@ import { seachNames } from "../repositories/seachUserRepository.js";
 
 async function seachUser(req, res) {
   const { nameUser } = req.params;
-
-  if (nameUser.length < 3) {
-    return res.status(404).send("Digite pelo menos 3 letras.");
-  }
   try {
-    const users = await seachNames(nameUser);
-
-    if(users.rowCount === 0){
-        return res.status(404).send("Nenhum usuário encontrado!");
+    if (nameUser.length < 3) {
+      return res.send([]);
     }
+    
+    const users = await seachNames(nameUser);
     
     return res.send(users.rows);
   } catch (e) {
