4481aa3471aed0b8d75bb8a0a2483fa2243fe26c
Caio-Coldebella <c232621@dac.unicamp.br>
2022-10-23 18:32:24 -0300

    rota get posts retorna também informações do usuário

--- schemas/publishSchema.js
+++ schemas/publishSchema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const publishSchema = joi.object({
     url: joi.string().required(),
-    complement: joi.string()
+    complement: joi.string().allow(null, '')
 });
 
 export default publishSchema;
