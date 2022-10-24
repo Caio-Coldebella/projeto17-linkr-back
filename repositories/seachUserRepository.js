3597ef469cd1bfaa0e6344416beeb7adce944c3e
isael lopes <isael.alves@turucui.ufpa.br>
2022-10-24 00:18:17 -0300

    update na busca por usuários

--- repositories/seachUserRepository.js
+++ repositories/seachUserRepository.js
@@ -2,7 +2,7 @@ import db from "../database/database.js";
 
 async function seachNames(nameUser) {
   return db.query(
-    `SELECT u.username, u."pictureUrl" FROM users u WHERE username LIKE '${nameUser}%'`
+    `SELECT u.id, u.username, u."pictureUrl" FROM users u WHERE username LIKE '${nameUser}%'`
   );
 }
 
