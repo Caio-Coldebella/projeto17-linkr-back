0606b9ea580806a701a744f266f3032f5bd0a6df
Maria Clara de Castro <mariaclarabfc.eng@gmail.com>
2022-10-24 16:31:32 -0300

    criando hashtags no post

--- controllers/publishController.js
+++ controllers/publishController.js
@@ -2,15 +2,33 @@ import publishRepository from "../repositories/publishRepository.js";
 import { findHashtagByName, createPostWithaHashtag, createNewHashtag } from "../repositories/hashtagRepository.js";
 
 export async function postPublish(req, res) {
-  const { url, complement } = req.body;
+  const { url, complement, topics } = req.body;
   const userId = res.locals.user[0].id;
   try {
+
     const validationURL =
       /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
     if (!url.match(validationURL)) {
       return res.status(422).send("URL invalida!");
     }
-    await publishRepository.postPublishPostByUserId(userId, url, complement);
+    const {rows: publishConfirm} = await publishRepository.postPublishPostByUserId(userId, url, complement);
+    const postId = publishConfirm[0].id;
+
+    if(topics.legth > 0){
+      topics.forEach(async topicName => {
+        const existinHashtag = await findHashtagByName(topicName);
+
+        if(existinHashtag.legth > 0) {
+          createPostWithaHashtag(postId, existinHashtag[0].id)
+        }
+
+        else{
+          const publishConfirm = await createNewHashtag(topicName)
+          const topicId = publishConfirm[0].id;
+          createPostWithaHashtag(postId, topicId);
+        }
+      });
+    }
     res.sendStatus(200);
   } catch (e) {
     console.log(e);
