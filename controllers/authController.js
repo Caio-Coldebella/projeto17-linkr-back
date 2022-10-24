e0567c225bd174449c1c86a29a909381e5e60c6d
Jessica <jessicavitorsantos@outlook.com>
2022-10-24 17:22:19 -0300

    logout e foto do perfil feitos

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,37 +15,47 @@ async function signUp (req, res) {
         const isValid = (await db.query(`SELECT * FROM users WHERE email = '${email}';`)).rows
         
         if ( isValid.length === 0) {
-             db.query(`INSERT INTO users (email, password, username, "pictureUrl") VALUES ('${email}', '${passwordHash}', '${username}', '${pictureUrl}' );`)
+             db.query('INSERT INTO users (email, password, username, "pictureUrl") VALUES ($1, $2, $3, $4 );', [email, passwordHash, username, pictureUrl])
        
             res.status(201).send("ok")
         } else {
-            res.status(409).send("usuário já cadastrado")
+            res.status(409).send("Usuário já cadastrado!")
         }
     } catch (error) {
-        console.log("deu  ruim")
-        res.status(422).send(error)
-        console.log(error)
+        res.status(422).send("Unprocessable Entity")
     }
 }
 
 async function signIn(req, res) {
     const {email, password} = req.body;
-
+    
     try {
 
-        const isValid = (await db.query(`SELECT * FROM users WHERE email = '${email}'`)).rows[0]
+        const isValid = (await db.query(`SELECT * FROM users WHERE email = '${email}';`)).rows[0]
 
         const passwordIsValid = bcrypt.compareSync(password, isValid.password)
 
         if ( isValid && passwordIsValid ) {
             const token = uuid();
-            await db.query(`INSERT INTO sessions ("userId", token) VALUES (${isValid.id}, '${token}');`)
-            res.status(200).send(`Bearer ${token}`)
+
+            let verify = (await db.query(`SELECT * FROM sessions WHERE "userId" = ${isValid.id};`)).rows;
+
+            if (verify.length === 0) {
+                await db.query(`INSERT INTO sessions ("userId", token) VALUES (${isValid.id}, 'Bearer ${token}');`)
+            } else {
+                await db.query(`DELETE FROM sessions WHERE "userId" = ${isValid.id}`)
+
+                await db.query(`INSERT INTO sessions ("userId", token) VALUES (${isValid.id}, 'Bearer ${token}');`)
+            }
+
+            res.status(200).send(token)
         } else {
-            res.stauts(401).send("email ou senha incorretos")
+            res.status(401).send("E-mail ou senha incorretos!")
         }
     } catch (error) {
-        res.status(422).send(error)
+        
+        res.status(422).send("Unprocessable Entity")
+        
     }
 }
 
